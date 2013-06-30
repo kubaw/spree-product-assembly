10ab8b226720a48a1945e399724febfc637bbaf1
Jakub Woźny <jakub@wozny.net.pl>
2013-06-30 21:34:44 +0200

    js fix

--- app/assets/javascripts/admin/product_assembly.js
+++ app/assets/javascripts/admin/product_assembly.js
@@ -3,19 +3,24 @@
  * https://github.com/spree/spree-product-assembly
  *
  */
-
-$("#searchtext").keypress(function (e) {
-  if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+$(document).ready(function() {
+  $("#searchtext").keypress(function (e) {
+    if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+      search_for_parts();
+      return false;
+    } else {
+      return true;
+    }
+  });
+  
+  $("#searchtext").delayedObserver(function(element, value) {
     search_for_parts();
-    return false;
-  } else {
-     return true;
-  }
+  }, 0.75);
+
+  subscribe_product_part_links();
+
 });
 
-$("#searchtext").delayedObserver(function(element, value) {
-  search_for_parts();
-}, 0.75);
 
 function subscribe_product_part_links()
 {
@@ -23,7 +28,7 @@ function subscribe_product_part_links()
     params = { count :  $("input", $(this).parent().parent()).val() };
     return make_post_request($(this), params);
   });
-  
+
   $("a.remove_admin_product_part_link").click(function(){
     return make_post_request($(this), {});
   });
@@ -34,10 +39,10 @@ function make_post_request(link, post_params)
   spinner = $("img.spinner", link.parent())
   spinner.show();
   $.post(link.attr("href"), post_params,
-    function (data, textStatus) { spinner.hide(); },
-    "script");
+         function (data, textStatus) { spinner.hide(); },
+         "script");
 
-  return false;
+         return false;
 }
 
-subscribe_product_part_links();
+
